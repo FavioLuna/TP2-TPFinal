dd533937848b0eb00049d18ae0b5f16bc9624b08
FavioLuna <fdluna99@gmail.com>
2022-06-10 18:17:05 -0300

    exporto y agrego a data-sorce Liga

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Liga } from "./entity/Liga"
 
 export const AppDataSource = new DataSource({
     type: "mongodb",
 database: "test",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Liga],
     migrations: [],
     subscribers: [],
 })
--- src/entity/Liga.ts
+++ src/entity/Liga.ts
@@ -1,7 +1,7 @@
 import { Entity, ObjectIdColumn, ObjectID, Column } from "typeorm"
 
 @Entity()
-export class User {
+export class Liga {
 
     @ObjectIdColumn()
     id: ObjectID
