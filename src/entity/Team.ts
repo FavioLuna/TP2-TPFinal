3604c6c3d6e9b28ea4fcbd171ec57136d6ddeb97
Luciano <rubin.luciano1@gmail.com>
2022-06-13 23:24:15 -0300

    added validations to Team entity

--- src/entity/Team.ts
+++ src/entity/Team.ts
@@ -1,6 +1,19 @@
 import { Entity, ObjectIdColumn, ObjectID, Column } from "typeorm"
+import {IsNotEmpty} from "class-validator"
 
 @Entity()
 export class Team{
     
+    @ObjectIdColumn()
+    id: ObjectID
+
+    @Column()
+    @IsNotEmpty()
+    name: string
+
+
+    @Column()
+    @IsNotEmpty()
+    img: string
+
 }
